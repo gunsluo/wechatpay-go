77f2b660c795dfc4355303e061b11a7803a9bc18
luoji <gunsluo@gmail.com>
2021-01-22 16:54:55 +0800

    define endpoints

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # wechatpay-go
-Wechat Pay SDK(V3) Write by Go.
+Wechat Pay SDK(V3) Write by Go. API V3 of Office document is [here](https://pay.weixin.qq.com/wiki/doc/apiv3/index.shtml)
---
+++ client.go
@@ -0,0 +1,26 @@
+// Copyright The Wechat Pay Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package wechatpay
+
+// Client is wechat pay client for api v3
+type Client struct {
+}
+
+// NewClient creates a new client with configuration from cfg.
+func NewClient(cfg Config) (*Client, error) {
+	c := &Client{}
+
+	return c, nil
+}
